b5e74d43e9ff48817ff6dccc47c5e04b9edd8061
sthallysson <sthallysson5@gmail.com>
2023-10-13 01:05:12 -0300

    style: changed comment

--- Atividades - Front/projetos/Modulo_Javascript/cadastro_de_pessoa/index.js
+++ Atividades - Front/projetos/Modulo_Javascript/cadastro_de_pessoa/index.js
@@ -1,4 +1,8 @@
-// Vamos criar um cadastro de pessoa com os seguintes dados: nome, salário, idade e se possui diploma. Antes de começar a codificação, identifiquem os tipos de cada variável. Em seguida, desenvolvam o código utilizando exemplos para cada variável que respeitem os tipos identificados. Certifiquem-se de que os valores correspondam aos tipos de dados corretos.
+/* 
+ Vamos criar um cadastro de pessoa com os seguintes dados: nome, salário, idade e se possui diploma. Antes de começar a 
+codificação, identifiquem os tipos de cada variável. Em seguida, desenvolvam o código utilizando exemplos para cada 
+variável que respeitem os tipos identificados. Certifiquem-se de que os valores correspondam aos tipos de dados corretos. 
+*/
 
 /* 
     Tipos das Variaveis
